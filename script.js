09b502c918660c916f680d89ca9b50615028a7a1
André Hoesch <45710373+ArgonH@users.noreply.github.com>
2026-07-18 21:52:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function setPage(image){
-
     document.getElementById("display").src = image;
+}
+
+
+// Service Worker für Offline-Betrieb registrieren
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker.register("service-worker.js")
+    .then(() => {
+        console.log("Service Worker erfolgreich registriert");
+    })
+    .catch(error => {
+        console.log("Service Worker Fehler:", error);
+    });
 
 }
